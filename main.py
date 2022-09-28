953fbf75e3c80536e892cd89b02aaac99ef8c402
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-09-28 11:33:48 -0600

    :v:

--- main.py
+++ main.py
@@ -10,8 +10,22 @@ def main():
     #glSphere(3, 1, -16, 0.5)
 
     #Creando al Snowman.
+    #Parte de abajo.
+    #Botón entre la parte de abajo y la media.
+    glSphere(2.3,      0, -12, 0.3, color(0.1, 0.5, 0.4)) #Esfera 3 color azul.
+
+    #Botón de hasta abajo.
+    glSphere(2.3,   1.5, -12, 0.5, color(0.1, 0.5, 0.4)) #Esfera 4 color azul.
+
     glSphere(3,        2, -16, 2, color(1, 0, 0)) #Esfera 1 color rojo.
+    
+    #Creando botones del tronco del muñeco.
+    glSphere(2.3,      -0.7, -12, 0.1, color(0.1, 0.5, 0.4)) #Esfera 3 color azul.
+    
+    #Tronco.
     glSphere(3.8,     -1, -20, 2, color(0, 1, 0)) #Esfera 2 color verde.
+    
+    #Cabeza.
     glSphere(3.45,   -3.5, -18, 1, color(0, 0, 1)) #Esfera 3 color azul.
 
     finish()
